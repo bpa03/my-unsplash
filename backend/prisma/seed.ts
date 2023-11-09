81dc497f85b36b27aea8512cadeca0b5de828dd7
bpa03 <baldassarepugliese@gmail.com>
2023-11-09 03:20:48 -0400

    feat (backend): Adding label field on image/photo table

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -5,18 +5,35 @@ const prisma = new PrismaClient()
 async function main (): Promise<void> {
   const baldassare = await prisma.user.create({
     data: {
-      email: 'baldassarepugliese@gmail.com',
+      email: 'baldassare@gmail.com',
       password: '12345678',
       name: 'Baldassare',
       lastname: 'Pugliese Alvarez',
       gender: 'MALE',
       Image: {
-        create: {
-          imageUrl: 'http.some.url.com'
-        }
+        create: [
+          {
+            imageUrl: 'https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?q=80&w=1384&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            description: 'super image label'
+          },
+          {
+            imageUrl: 'https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?q=80&w=1384&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            description: 'super image label'
+          },
+          {
+            imageUrl: 'https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?q=80&w=1384&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            description: 'super image label'
+          },
+          {
+            imageUrl: 'https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?q=80&w=1384&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            description: 'super image label'
+          }
+        ]
       }
     }
   })
+
+  // Images
   console.log({baldassare})
 }
 
